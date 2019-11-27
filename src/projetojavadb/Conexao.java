1737cacf0dd6abb4c0a1cec991e05538ecb06151
Tyreum <gabriel.kmn@gmail.com>
2019-11-26 22:07:55 -0300

    aaaaaaaaaaaaaa

--- src/projetojavadb/Conexao.java
+++ src/projetojavadb/Conexao.java
@@ -3,5 +3,51 @@ package projetojavadb;
 import java.sql.*;
 
 public class Conexao {
-    
+
+    public static Connection abrirConexao() {
+        Connection con = null;
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+
+            String url = "";
+
+            url += "jdbc:mysql//127.0.0.1:3307/estacionamento?";
+            url += "user=root&password=etecia";
+
+            con = DriverManager.getConnection(url);
+            
+            System.out.println("Conexão aberta mec");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+
+        }
+
+
+        return con;
+    }
+
+    public static Connection fecharConexao() {
+        Connection con = null;
+
+        try {
+            con.close();
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+
+        }
+
+        return con;
+    }
 }
